5419de42b8668dc64a9699bd4f31bd6ccf240343
rusfirefly <fesov_86@mail.ru>
2024-03-12 16:14:54 +0300

    update

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,59 +6,68 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private HingeJoint[] _wheels;
     [SerializeField] private HingeJoint _cabin;
-
-    [SerializeField] private float _speed;
     [SerializeField] private float _speedTurn;
-    [SerializeField] private Mover _move;
+   
+    private Vector3 _axis;
 
     private void Start()
     {
-            
+
     }
 
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        /* float x = Input.GetAxis("Horizontal");
+         float z = Input.GetAxis("Vertical");
 
-        float turn = Input.GetAxis("Turn");
+         float turn = Input.GetAxis("Turn");
 
-        
+         if (turn > 0.1f)
+         {
+             CabinTurnLeft();
+         }
+         if (turn < 0)
+         {
+             CabinTurnRight();
+         }
 
-        if (turn > 0.1f)
-        {
-            CabinTurnLeft();
-        }
-        if (turn < 0)
-        {
-            CabinTurnRight();
-        }
-
-        if(turn == 0f)
-        {
-            Debug.Log("stop");
-            TurnStop();
-        }
+         if (turn == 0f)
+         {
+             TurnStop();
+         }*/
     }
 
     private void CabinTurnLeft()
     {
-        _cabin.axis = new Vector3(0,-1,0);
-        Turn();
+        _axis = _cabin.axis;
+        
+        if (_axis.y > 0)
+        {
+            _axis.y = -1;
+            _cabin.axis = _axis;
+            Turn();
+        }
+       
     }
 
     private void CabinTurnRight()
     {
-        _cabin.axis = new Vector3(0, 1, 0);
-        Turn();
+        _axis = _cabin.axis;
+        if (_axis.y < 0 || _axis.y == 1)
+        {
+            _axis.y = 1;
+            _cabin.axis = _axis;
+            Turn();
+        }
+        
     }
 
     private void TurnStop()
     {
-        _cabin.useMotor = false;
         JointMotor motor = _cabin.motor;
         motor.force = 0f;
         _cabin.motor = motor;
+        _cabin.useMotor = false;
     }
 
 
